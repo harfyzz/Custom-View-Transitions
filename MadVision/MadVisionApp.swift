a0071e86a30e673a7ebfe3ca61ae52da61ee307e
Afeez <harfyzz@gmail.com>
2024-09-23 12:07:10 +0100

    Initial Commit

---
+++ MadVision/MadVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MadVisionApp.swift
+//  MadVision
+//
+//  Created by Afeez Yunus on 23/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct MadVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MadVisionTests/MadVisionTests.swift
@@ -0,0 +1,17 @@
+//
+//  MadVisionTests.swift
+//  MadVisionTests
+//
+//  Created by Afeez Yunus on 23/09/2024.
+//
+
+import Testing
+@testable import MadVision
+
+struct MadVisionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
